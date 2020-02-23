9caa68bd757c0027f5ee47dd570a350c211dd7e5
Mr.m <Mr.m>
2020-02-23 13:58:38 +0800

    search bug fixed.

--- search.php
+++ search.php
@@ -18,6 +18,7 @@ define('IN_PHPBB', true);
 $phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
 $phpEx = substr(strrchr(__FILE__, '.'), 1);
 include($phpbb_root_path . 'common.' . $phpEx);
+include($phpbb_root_path . 'api_config.' . $phpEx);
 
 // Start session management
 $user->session_begin();
@@ -1249,6 +1250,9 @@ if ($keywords || $author || $author_id || $search_id || $submit)
 			);
 			extract($phpbb_dispatcher->trigger_event('core.search_modify_tpl_ary', compact($vars)));
 
+			// to fixed video url for iframe html render
+			$tpl_ary['MESSAGE'] = str_replace('src="/v/', 'src="'.$apiConfig['v_url'].'/v/', $tpl_ary['MESSAGE']);
+			
 			$template->assign_block_vars('searchresults', $tpl_ary);
 
 			if ($show_results == 'topics')
